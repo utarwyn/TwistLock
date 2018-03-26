1be040c64605e71aed9d289f322beb69f8f6c8f2
Florian <florian.cruchon@etu.univ-lehavre.fr>
2018-03-26 10:30:10 +0200

    Class Joueur modifié Class TwistLock crée

--- src/metier/TwistLock.java
+++ src/metier/TwistLock.java
@@ -2,6 +2,15 @@ package metier;
 
 public class TwistLock {
 
+    Joueur joueur;
+    int coin;
 
+    public TwistLock(Joueur joueur, int coin){
+        this.joueur = joueur;
+        this.coin = coin;
+    }
 
+    public Joueur getJoueur() { return joueur; }
+
+    public int getCoin() { return coin; }
 }
