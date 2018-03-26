aa6314e706744f4249ea99782c2ae2d38c8e88d6
Florian <florian.cruchon@etu.univ-lehavre.fr>
2018-03-26 10:30:10 +0200

    Class Joueur modifié Class TwistLock crée

--- src/metier/Joueur.java
+++ src/metier/Joueur.java
@@ -1,32 +1,31 @@
 package metier;
 
 public class Joueur {
-
     int id, score;
-    int twist_lock = 20;
+    int nbTwistLock = 20;
     String couleur, nom;
 
-    public Joueur(int id, String couleur, String nom, int score, int twist_lock){
+    public Joueur(int id, String couleur, String nom, int score, int nbTwistLock){
         this.id = id;
         this.couleur = couleur;
         this.nom = nom;
         this.score = score;
-        this.twist_lock = twist_lock;
+        this.nbTwistLock = nbTwistLock;
     }
 
     public int getId() { return id; }
     public String getCouleur() { return couleur; }
     public String getNom() { return nom; }
     public int getScore() { return score; }
-    public int getTwist_lock() { return twist_lock; }
+    public int getNbTwistLockwistLock() { return nbTwistLock; }
 
     public void setId(int id) { this.id = id; }
     public void setCouleur(String couleur) { this.couleur = couleur; }
     public void setNom(String nom) { this.nom = nom; }
     public void setScore(int score) { this.score = score; }
-    public void setTwist_lock(int twist_lock) { this.twist_lock = twist_lock; }
+    public void setNbTwistLock(int nbTwistLock) { this.nbTwistLock = nbTwistLock; }
 
     public void penalite(){
-        setTwist_lock(this.twist_lock-1);
+        setNbTwistLock(this.nbTwistLock-1);
     }
 }
