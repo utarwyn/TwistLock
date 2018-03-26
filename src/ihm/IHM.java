67d4db12080bf567ca8afed05ede0959f9454c46
Félix DENIS <felix.denis@outlook.com>
2018-03-26 10:17:06 +0200

    ihm

--- src/ihm/IHM.java
+++ src/ihm/IHM.java
@@ -1,9 +1,23 @@
 package ihm;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class IHM extends Application
 {
+    
+    /**
+     * possède les éléments graphiques
+     */
+    private Group group = new Group( );
+    
+    /**
+     * possède la fenêtre graphique
+     */
+    private Scene scene = new Scene( group , 800 , 800 , Color.LIGHTBLUE );
+    
     public IHM( )
     {
     
