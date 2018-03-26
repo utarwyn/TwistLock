a3f21f25afdc5809e72b359639182fb4d7ce5fa8
Félix DENIS <felix.denis@outlook.com>
2018-03-26 10:08:16 +0200

    ihm

---
+++ src/ihm/IHM.java
@@ -0,0 +1,22 @@
+package ihm;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class IHM extends Application
+{
+    public IHM( )
+    {
+    
+    }
+    
+    @Override
+    public void start( Stage primaryStage ) throws Exception
+    {
+    
+    }
+    
+    public static void main( String[] args )
+    {
+        Application.launch( IHM.class , args );
+    }
+}
