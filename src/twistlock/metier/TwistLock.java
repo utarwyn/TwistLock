a2bcfd11001e99e70d05d2fd8b4e1431234d4303
Florian <florian.cruchon@etu.univ-lehavre.fr>
2018-03-26 10:38:17 +0200

    Class TwistLock Modifié

--- src/twistlock/metier/TwistLock.java
+++ src/twistlock/metier/TwistLock.java
@@ -5,12 +5,9 @@ public class TwistLock {
     Joueur joueur;
     int coin;
 
-    public TwistLock(Joueur joueur, int coin){
+    public TwistLock(Joueur joueur){
         this.joueur = joueur;
-        this.coin = coin;
     }
 
     public Joueur getJoueur() { return joueur; }
-
-    public int getCoin() { return coin; }
 }
