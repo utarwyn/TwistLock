09528468a6e6e6ed04980af9a2401588083297a4
Félix DENIS <felix.denis@outlook.com>
2018-03-26 10:50:50 +0200

    controleur pubiqueeeeeeeeeeeeeeeeeeeeee

--- src/twistlock/Controleur.java
+++ src/twistlock/Controleur.java
@@ -3,18 +3,20 @@ package twistlock;
 import twistlock.ihm.IHM;
 import twistlock.metier.Metier;
 
-public class Controleur {
-
-	private Metier metier;
-
-	private IHM ihm;
-
-	private Controleur() {
-
-	}
-
-    public static void main( String[] args ) {
-        System.out.println("twistlock/test");
+public class Controleur
+{
+    
+    private Metier metier;
+    
+    private IHM ihm;
+    
+    public Controleur( )
+    {
+    
     }
-
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "twistlock/test" );
+    }
 }
