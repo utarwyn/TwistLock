b572ccb16dd2c2039688d3de623b89aca18641b9
Félix DENIS <felix.denis@outlook.com>
2018-03-28 23:13:27 +0200

    ajout de fonctionnalitées pour choisir l'adresse IP des cartes réseaux ajout des joueurs en cours de connexion

--- src/twistlock/ihm/ConnexionClient.java
+++ src/twistlock/ihm/ConnexionClient.java
@@ -3,146 +3,219 @@ import twistlock.Controleur;
 
 import javax.swing.*;
 import java.awt.*;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class ConnexionClient extends JFrame
 {
-    private Controleur controleur;
-    
-    private JLabel jLabelTitre;
-    private JLabel jLabelAdresseIP;
-    private JLabel jLabelPort;
-    private JLabel jLabelJ1;
-    private JLabel jLabelJ2;
-    private JLabel jLabelJ3;
-    private JLabel jLabelJ4;
-    
-    private JButton buttonValider;
-    private JButton buttonQuitter;
-    private int     nbJoueurs;
-    private int     lignes;
-    private int     colonnes;
-    private int     nbTwistlocks;
-    private int     portConnexion;
-    
-    public ConnexionClient( Controleur controleur )
-    {
-        this.controleur = controleur;
-        
-        this.setTitle( "Jeu des Twistlocks - Connexion des Joueurs" );
-        this.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-        this.setSize( 1200 , 800 );
-        
-        //prépare les éléments graphique
-        preparer( );
-        
-        //ajoute les élments graphiques
-        remplir( );
-        
-        this.setLocationRelativeTo( null );
-        
-        this.setVisible( true );
-    }
-    
-    private void preparer( )
-    {
-        
-        jLabelTitre = new JLabel( "Configurez votre partie" );
-        
-        try {
-            jLabelAdresseIP = new JLabel( "Adresse IP serveur : " + InetAddress.getLocalHost( ).getHostAddress( ) );
-        } catch( UnknownHostException e ) {
-            e.printStackTrace( );
-        }
-        
-        jLabelPort = new JLabel( "Port de connexion : " + controleur.getServeur().getPortConnexion() );
-        
-        jLabelJ1 = new JLabel( "Joueur 1 : " );
-        jLabelJ2 = new JLabel( "Joueur 2 : " );
-        //if( controleur.get )jLabelJ3 = new JLabel( "Joueur 3 : " );
-        jLabelJ4 = new JLabel( "Joueur 4 : " );
-        
-        //pour quitter
-        buttonQuitter = new JButton( "Quitter" );
-        buttonQuitter.addActionListener( e -> System.exit( 0 ) );
-        
-        //pour passer au jeu
-        buttonValider = new JButton( "Fermer la fenêtre" );
-        buttonValider.addActionListener( e -> {
-            this.dispose();
-        } );
-    }
-    
-    /**
-     * Remplie l'interface graphique et met en forme les éléments
-     */
-    private void remplir( )
-    {
-        this.setLayout( new GridBagLayout( ) );
-        
-        GridBagConstraints gridBagConstraints = new GridBagConstraints( );
-        
-        gridBagConstraints.insets = new Insets( 10 , 10 , 10 , 10);
-        gridBagConstraints.anchor = GridBagConstraints.LINE_START;
-        
-        gridBagConstraints.gridwidth = 1;
-        this.add( jLabelTitre , gridBagConstraints );
-        
-        gridBagConstraints.gridwidth = 1;
-        
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridx = 0;
-        this.add( jLabelPort , gridBagConstraints );
-
-        gridBagConstraints.gridy = 2;
-        this.add( jLabelAdresseIP , gridBagConstraints );
-        
-        gridBagConstraints.anchor = GridBagConstraints.CENTER;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.gridx = 0;
-        this.add( buttonQuitter , gridBagConstraints );
-        
-        gridBagConstraints.gridx = 1;
-        this.add( buttonValider , gridBagConstraints );
-    }
-    
-    /**
-     * Lance le jeu et ouvre la fenêtre avec le plateau du jeu Ajoute les joueurs
-     */
-    private void lancer( )
-    {
-        this.dispose( );
-    }
-    
-    /**
-     * fenêtre d'erreur
-     *
-     * @param message
-     *         a afficher dans la fenêtre d'erreur
-     */
-    private void erreur( String message )
-    {
-        JOptionPane.showMessageDialog( null , message );
-    }
-    
-    public JLabel getjLabelJ1( )
-    {
-        return jLabelJ1;
-    }
-    
-    public JLabel getjLabelJ2( )
-    {
-        return jLabelJ2;
-    }
-    
-    public JLabel getjLabelJ3( )
-    {
-        return jLabelJ3;
-    }
-    
-    public JLabel getjLabelJ4( )
-    {
-        return jLabelJ4;
-    }
+	private Controleur controleur;
+	
+	private JLabel jLabelTitre;
+	private JLabel jLabelAdresseIP;
+	private JLabel jLabelAdresseIPValue;
+	private JLabel jLabelPort;
+	private JLabel jLabelPortValue;
+	private JLabel jLabelJ1;
+	private JLabel jLabelJ1Value;
+	private JLabel jLabelJ2;
+	private JLabel jLabelJ2Value;
+	private JLabel jLabelJ3;
+	private JLabel jLabelJ3Value;
+	private JLabel jLabelJ4;
+	private JLabel jLabelJ4Value;
+	
+	private JButton buttonValider;
+	private JButton buttonQuitter;
+	
+	private int nbJoueurs;
+	
+	public ConnexionClient( Controleur controleur , int nbJoueurs )
+	{
+		this.controleur = controleur;
+		this.nbJoueurs = nbJoueurs;
+		
+		this.setTitle( "Jeu des Twistlocks - Connexion des Joueurs" );
+		this.setSize( 1200 , 800 );
+		
+		//prépare les éléments graphique
+		preparer( );
+		
+		//ajoute les élments graphiques
+		remplir( );
+		
+		this.setLocationRelativeTo( null );
+		
+		this.setVisible( true );
+	}
+	
+	private void preparer( )
+	{
+		
+		jLabelTitre = new JLabel( "Configurez votre partie" );
+		
+		jLabelAdresseIP = new JLabel( "Adresse de connexion : " );
+		jLabelAdresseIPValue = new JLabel( controleur.getServeur( ).getAdresseIP( ) );
+		
+		jLabelPort = new JLabel( "Port de connexion : " );
+		jLabelPortValue = new JLabel( String.valueOf( controleur.getServeur( ).getPortConnexion( ) ) );
+		
+		jLabelJ1 = new JLabel( "Joueur 1 : " );
+		jLabelJ1Value = new JLabel( );
+		jLabelJ2 = new JLabel( "Joueur 2 : " );
+		jLabelJ2Value = new JLabel( );
+		jLabelJ3 = new JLabel( "Joueur 3 : " );
+		jLabelJ3Value = new JLabel( );
+		jLabelJ4 = new JLabel( "Joueur 4 : " );
+		jLabelJ4Value = new JLabel( );
+		
+		//pour quitter
+		buttonQuitter = new JButton( "Quitter" );
+		buttonQuitter.addActionListener( e -> System.exit( 0 ) );
+		
+		//pour passer au jeu
+		buttonValider = new JButton( "Fermer la fenêtre" );
+		buttonValider.addActionListener( e -> {
+			this.dispose( );
+		} );
+	}
+	
+	/**
+	 * Remplie l'interface graphique et met en forme les éléments
+	 */
+	private void remplir( )
+	{
+		this.setLayout( new GridBagLayout( ) );
+		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints( );
+		
+		gridBagConstraints.insets = new Insets( 10 , 10 , 10 , 10 );
+		gridBagConstraints.anchor = GridBagConstraints.CENTER;
+		
+		gridBagConstraints.gridwidth = 2;
+		this.add( jLabelTitre , gridBagConstraints );
+		
+		gridBagConstraints.gridwidth = 1;
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+		gridBagConstraints.gridy = 1;
+		gridBagConstraints.gridx = 0;
+		this.add( jLabelPort , gridBagConstraints );
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+		gridBagConstraints.gridx = 1;
+		this.add( jLabelPortValue , gridBagConstraints );
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+		gridBagConstraints.gridy = 2;
+		gridBagConstraints.gridx = 0;
+		this.add( jLabelAdresseIP , gridBagConstraints );
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+		gridBagConstraints.gridx = 1;
+		this.add( jLabelAdresseIPValue , gridBagConstraints );
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+		gridBagConstraints.gridy = 3;
+		gridBagConstraints.gridx = 0;
+		this.add( jLabelJ1 , gridBagConstraints );
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+		gridBagConstraints.gridx = 1;
+		this.add( jLabelJ1Value , gridBagConstraints );
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+		gridBagConstraints.gridy = 4;
+		gridBagConstraints.gridx = 0;
+		this.add( jLabelJ2 , gridBagConstraints );
+		
+		gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+		gridBagConstraints.gridx = 1;
+		this.add( jLabelJ2Value , gridBagConstraints );
+		
+		if( nbJoueurs > 2 ) {
+			gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+			gridBagConstraints.gridy = 5;
+			gridBagConstraints.gridx = 0;
+			this.add( jLabelJ3 , gridBagConstraints );
+			
+			gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+			gridBagConstraints.gridx = 1;
+			this.add( jLabelJ3Value , gridBagConstraints );
+		}
+		
+		if( nbJoueurs > 3 ) {
+			gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+			gridBagConstraints.gridy = 6;
+			gridBagConstraints.gridx = 0;
+			this.add( jLabelJ4 , gridBagConstraints );
+			
+			gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+			gridBagConstraints.gridx = 1;
+			this.add( jLabelJ4Value , gridBagConstraints );
+		}
+		
+		gridBagConstraints.anchor = GridBagConstraints.CENTER;
+		gridBagConstraints.gridy = 7;
+		gridBagConstraints.gridx = 0;
+		this.add( buttonQuitter , gridBagConstraints );
+		
+		gridBagConstraints.gridx = 1;
+		this.add( buttonValider , gridBagConstraints );
+	}
+	
+	/**
+	 * Lance le jeu et ouvre la fenêtre avec le plateau du jeu Ajoute les joueurs
+	 */
+	private void lancer( )
+	{
+		this.dispose( );
+	}
+	
+	/**
+	 * fenêtre d'erreur
+	 *
+	 * @param message
+	 * 		a afficher dans la fenêtre d'erreur
+	 */
+	private void erreur( String message )
+	{
+		JOptionPane.showMessageDialog( null , message );
+	}
+	
+	public void setJLabel( int i , String nom )
+	{
+		switch( i ) {
+			case 1:
+				setjLabelJ1Value( nom );
+				break;
+			case 2:
+				setjLabelJ2Value( nom );
+				break;
+			case 3:
+				setjLabelJ3Value( nom );
+				break;
+			case 4:
+				setjLabelJ4Value( nom );
+				break;
+			default:
+		}
+	}
+	
+	public void setjLabelJ1Value( String jLabelJ1Value )
+	{
+		this.jLabelJ1Value.setText( jLabelJ1Value );
+	}
+	
+	public void setjLabelJ2Value( String jLabelJ2Value )
+	{
+		this.jLabelJ2Value.setText( jLabelJ2Value );
+	}
+	
+	public void setjLabelJ3Value( String jLabelJ3Value )
+	{
+		this.jLabelJ3Value.setText( jLabelJ3Value );
+	}
+	
+	public void setjLabelJ4Value( String jLabelJ4Value )
+	{
+		this.jLabelJ4Value.setText( jLabelJ4Value );
+	}
 }
--- src/twistlock/net/ClientTest.java
+++ src/twistlock/net/ClientTest.java
@@ -12,63 +12,64 @@ import java.net.InetAddress;
  */
 public class ClientTest
 {
-    private DatagramSocket ds;
-    
-    private int port;
-    
-    private String adresse;
-    
-    public ClientTest( ) throws IOException
-    {
-        ds = new DatagramSocket( );
-        BufferedReader entreeClavier = new BufferedReader( new InputStreamReader( System.in ) );
-    
-        System.out.println( "Adresse du serveur : " );
-        
-        adresse = entreeClavier.readLine();
-        
-        System.out.println( "Port du serveur    : " );
-        
-        port = Integer.parseInt( entreeClavier.readLine() );
-        
-        System.out.println( "Nouvelle partie de TwistLock\n" + "\n" + "Vous pouvez envoyer les messages :\n" + "    MAP : donne la map du plateau\n" +
-                            "    ??? : permet de jouer un coup avec \n" + "              1 - ligne avec un numéro de 1 à 9\n" +
-                            "              2 - colonne avec une lettre A à I\n" + "              3 - le coin de 1 à 4\n" +
-                            "                      1 - pour en haut à gauche\n" + "                      2 - pour en haut à droite\n" +
-                            "                      3 - pour en bas  à droite\n" + "                      4 - pour en bas  à gauche\n" +
-                            "    q   : pour quitter\n" );
-        
-        new Thread( ( ) -> {
-            do {
-                DatagramPacket msg = new DatagramPacket( new byte[ 1024 ] , 1024 );
-                try {
-                    ds.receive( msg );
-                } catch( IOException e ) {
-                    e.printStackTrace( );
-                }
-
-                String message = new String( msg.getData( ) ).trim( );
-
-                if ( message.equals("KILL") ) System.exit( 0 );
+	private DatagramSocket ds;
+	
+	private int port;
+	
+	private String adresse;
+	
+	public ClientTest( ) throws IOException
+	{
+		ds = new DatagramSocket( );
+		BufferedReader entreeClavier = new BufferedReader( new InputStreamReader( System.in ) );
+		
+		System.out.println( "Adresse du serveur : " );
+		
+		adresse = entreeClavier.readLine( );
+		
+		System.out.println( "Port du serveur    : " );
+		
+		port = Integer.parseInt( entreeClavier.readLine( ) );
+		
+		System.out.println( "Nouvelle partie de TwistLock\n" + "\n" + "Vous pouvez envoyer les messages :\n" + "    MAP : donne la map du " +
+		                    "plateau\n" +
+		                    "    ??? : permet de jouer un coup avec \n" + "              1 - ligne avec un numéro de 1 à 9\n" +
+		                    "              2 - colonne avec une lettre A à I\n" + "              3 - le coin de 1 à 4\n" +
+		                    "                      1 - pour en haut à gauche\n" + "                      2 - pour en haut à droite\n" +
+		                    "                      3 - pour en bas  à droite\n" + "                      4 - pour en bas  à gauche\n" +
+		                    "    q   : pour quitter\n" );
+		
+		new Thread( ( ) -> {
+			do {
+				DatagramPacket msg = new DatagramPacket( new byte[ 1024 ] , 1024 );
+				try {
+					ds.receive( msg );
+				} catch( IOException e ) {
+					e.printStackTrace( );
+				}
+				
+				String message = new String( msg.getData( ) ).trim( );
+				
+				if( message.equalsIgnoreCase( "KILL" ) ) System.exit( 0 );
 				else System.out.println( "\n" + message );
-            } while( ds != null );
-        } ).start( );
-    
-        System.out.print( "\nVotre nom d'équipe  : " );
-        
-        do {
-            String message = entreeClavier.readLine( );
-            if( message.equalsIgnoreCase( "q" ) ) ds = null;
-            else {
-                DatagramPacket envoi = new DatagramPacket( message.getBytes( ) , message.length( ) , InetAddress.getByName( adresse ) , port );
-                ds.send( envoi );
-            }
-        } while( ds != null );
-        System.exit( 0 );
-    }
-    
-    public static void main( String args[] ) throws Exception
-    {
-        new ClientTest( );
-    }
+			} while( ds != null );
+		} ).start( );
+		
+		System.out.print( "\nVotre nom d'équipe  : " );
+		
+		do {
+			String message = entreeClavier.readLine( );
+			if( message.equalsIgnoreCase( "q" ) ) ds = null;
+			else {
+				DatagramPacket envoi = new DatagramPacket( message.getBytes( ) , message.length( ) , InetAddress.getByName( adresse ) , port );
+				ds.send( envoi );
+			}
+		} while( ds != null );
+		System.exit( 0 );
+	}
+	
+	public static void main( String args[] ) throws Exception
+	{
+		new ClientTest( );
+	}
 }
