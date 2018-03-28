54659d24926a69209efe287cac52df98ca89f46e
Maxime Malgorn <maxime.malgorn@laposte.net>
2018-03-28 14:24:32 +0200

    Transformation du serveur en Thread

--- src/twistlock/net/Serveur.java
+++ src/twistlock/net/Serveur.java
@@ -8,7 +8,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
-public class Serveur {
+public class Serveur extends Thread {
 
 	private Controleur controleur;
 
@@ -35,39 +35,42 @@ public class Serveur {
 		return nbJoueurs;
 	}
 
+	@Override
+	public void run() {
+		String messageRecu = "";
+
+		for(int i = 1; i <= nbJoueurs; i++)
+		{
+			DatagramPacket msg = new DatagramPacket(new byte[100], 100);
+
+			try{
+				System.out.println("salut");
+				datagramSocket.receive( msg );
+			} catch( IOException e ) {
+				e.printStackTrace( );
+			}
+
+
+			messageEnvoie = "";
+			String nom = new String( msg.getData( ) ).trim( );
+			messageEnvoie += ( i + "-Bonjour Equipe " + new String( msg.getData( ) ).trim( ) );
+			messageEnvoie += ( "\nVous etes le Joueur " + 1 + " (ROUGE)" ); //TODO: Doit changer la couleur
+			new Joueur( i , nom , 20 ); //TODO? : ajout Joueur autre
+
+		}
+
+		//TODO : Partie JEU
+		datagramSocket.close();
+	}
+
 	private void lancer() {
 		try {
-			datagramSocket = new DatagramSocket(this.portConnexion);
+			this.datagramSocket = new DatagramSocket(this.portConnexion);
+			// Démarrage du Thread du serveur ...
+			this.start();
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
-		DatagramPacket msg = new DatagramPacket(new byte[100], 100);
-
-		new Thread(() -> {
-            String messageRecu = "";
-            for(int i=1;i<=nbJoueurs;i++)
-            {
-                  try{
-                    System.out.println("salut");
-                      datagramSocket.receive( msg );
-                  } catch( IOException e ) {
-                      e.printStackTrace( );
-                  }
-
-
-                  messageEnvoie = "";
-                  String nom = new String( msg.getData( ) ).trim( );
-                  messageEnvoie += ( i + "-Bonjour Equipe " + new String( msg.getData( ) ).trim( ) );
-                  messageEnvoie += ( "\nVous etes le Joueur " + 1 + " (ROUGE)" ); //TODO: Doit changer la couleur
-                new Joueur( i , nom , 20 ); //TODO? : ajout Joueur autre
-
-            }
-
-            //TODO : Partie JEU
-        }).start();
-
-
-		datagramSocket.close();
 	}
 
 }
