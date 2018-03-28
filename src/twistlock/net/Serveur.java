b4cfe45da3c371558e0c70bfa65c906be3434939
Florian <florian.cruchon@etu.univ-lehavre.fr>
2018-03-28 14:12:39 +0200

    Class Serveur(non fonctionelle)

--- src/twistlock/net/Serveur.java
+++ src/twistlock/net/Serveur.java
@@ -1,7 +1,9 @@
 package twistlock.net;
 
 import twistlock.Controleur;
+import twistlock.metier.Joueur;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
@@ -11,6 +13,11 @@ public class Serveur {
 	private Controleur controleur;
 
 	private int portConnexion, nbJoueurs;
+
+	private DatagramSocket datagramSocket;
+	private DatagramPacket datagramPacketRecu, datagramPacketEnvoie;
+	private String messageRecu;
+	private String messageEnvoie;
 
 	public Serveur(Controleur controleur, int portConnexion, int nbJoueurs) {
 		this.controleur = controleur;
@@ -29,33 +36,38 @@ public class Serveur {
 	}
 
 	private void lancer() {
-		DatagramSocket ds = null;
 		try {
-			ds = new DatagramSocket(this.portConnexion);
+			datagramSocket = new DatagramSocket(this.portConnexion);
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 		DatagramPacket msg = new DatagramPacket(new byte[100], 100);
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				String messageRecu = "";
-				do {
-//                    ds.receive( msg );
-//                    String texteReponse = "";
-//                    String nom          = new String( msg.getData( ) ).trim( );
-//                    texteReponse += ( i + "-Bonjour Equipe " + new String( msg.getData( ) ).trim( ) );
-//                    texteReponse += ( "\nVous etes le Joueur " + i + " (ROUGE)" );
-//                    new Joueur( i , nom , 20 );
-//                    DatagramPacket reponse = new DatagramPacket( texteReponse.getBytes( ) , texteReponse.length( ) , msg.getAddress( ) , msg.getPort( ) );
-//                    ds.send( reponse );
-				} while (true);
-			}
-		}).run();
+		new Thread(() -> {
+            String messageRecu = "";
+            for(int i=1;i<=nbJoueurs;i++)
+            {
+                  try{
+                    System.out.println("salut");
+                      datagramSocket.receive( msg );
+                  } catch( IOException e ) {
+                      e.printStackTrace( );
+                  }
 
 
-		ds.close();
+                  messageEnvoie = "";
+                  String nom = new String( msg.getData( ) ).trim( );
+                  messageEnvoie += ( i + "-Bonjour Equipe " + new String( msg.getData( ) ).trim( ) );
+                  messageEnvoie += ( "\nVous etes le Joueur " + 1 + " (ROUGE)" ); //TODO: Doit changer la couleur
+                new Joueur( i , nom , 20 ); //TODO? : ajout Joueur autre
+
+            }
+
+            //TODO : Partie JEU
+        }).start();
+
+
+		datagramSocket.close();
 	}
 
 }
