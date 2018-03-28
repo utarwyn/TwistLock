a694e54c5415c0da4d0ebf5bbb98c32ef41f6598
Félix DENIS <felix.denis@outlook.com>
2018-03-28 15:36:38 +0200

    clienttest

--- src/twistlock/net/ClientTest.java
+++ src/twistlock/net/ClientTest.java
@@ -1,6 +1,7 @@
 package twistlock.net;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -14,30 +15,31 @@ public class ClientTest
         BufferedReader entreeClavier = new BufferedReader( new InputStreamReader( System.in ) );
         
         System.out.println( "Nouvelle partie de TwistLock\n" + "\n" + "Vous pouvez envoyer les messages :\n" + "    MAP : donne la map du plateau\n" +
-                            "    ??? : permet de jouer un coup avec " + "              1 - ligne avec un numéro de 1 à 9\n" +
+                            "    ??? : permet de jouer un coup avec \n" + "              1 - ligne avec un numéro de 1 à 9\n" +
                             "              2 - colonne avec une lettre A à I\n" + "              3 - le coin de 1 à 4\n" +
                             "                      1 - pour en haut à gauche\n" + "                      2 - pour en haut à droite\n" +
                             "                      3 - pour en bas  à droite\n" + "                      4 - pour en bas  à gauche\n" +
                             "    q   : pour quitter\n" );
-        
+        new Thread( ( ) -> {
+            do {
+                DatagramPacket msg = new DatagramPacket( new byte[ 1024 ] , 1024 );
+                try {
+                    ds.receive( msg );
+                } catch( IOException e ) {
+                    e.printStackTrace( );
+                }
+                System.out.println( new String( msg.getData( ) ).trim( ) );
+            }while( true );
+            
+        } ).start( );
         do {
             String message = entreeClavier.readLine( );
             
             if( message.equalsIgnoreCase( "q" ) ) ds.close( );
             else {
-                DatagramPacket envoi = new DatagramPacket( message.getBytes( ) , message.length( ) , InetAddress.getByName( "localhost" ) , 2684 );
+                DatagramPacket envoi = new DatagramPacket( message.getBytes( ) , message.length( ) , InetAddress.getByName( "127.0.0.1" ) , 2684 );
                 ds.send( envoi );
             }
-            new Thread( new Runnable( ) {
-                @Override
-                public void run( )
-                {
-                
-                }
-            } ).start();
-			DatagramPacket msg = new DatagramPacket(new byte[100], 100);
-			ds.receive(msg);
-			System.out.println(new String(msg.getData()));
-        } while( ds.isConnected( ) );
+        } while( true);
     }
 }
