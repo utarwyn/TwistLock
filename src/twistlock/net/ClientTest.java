bc149dafeb6aadff186697674613cd15e6f78c9e
Utarwyn <utarwyn@utaria.fr>
2018-03-28 15:08:09 +0200

    Ajout Client test

---
+++ src/twistlock/net/ClientTest.java
@@ -0,0 +1,25 @@
+package twistlock.net;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ClientTest {
+	public static void main(String args[]) throws Exception {
+		DatagramSocket ds = new DatagramSocket();
+		BufferedReader entreeClavier = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			String message = entreeClavier.readLine();
+
+			DatagramPacket envoi = new DatagramPacket(message.getBytes(), message.length(), InetAddress.getByName("localhost"), 2684);
+			ds.send(envoi);
+			System.out.println("envoi de " + message);
+			/*DatagramPacket msg = new DatagramPacket(new byte[100], 100);
+			ds.receive(msg);
+			System.out.println(new String(msg.getData()));*/
+		}
+	}
+}
